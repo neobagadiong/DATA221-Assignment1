cbc50976d537f9d3e8f6090c16bdcd8f4f9d1494
neo <neo.bagadiong02@gmail.com>
2026-01-30 20:22:49 -0700

    Added comment and fixed printout statement

--- Question4.py
+++ Question4.py
@@ -1,6 +1,17 @@
+'''
+Question 4: Sorted Search with Conditions
+Given a list of random values between 0 and 1 and a random value x:
+    • Sort the list.
+    • Find all indices where the list value is greater than or equal to x.
+    • Print the sorted list, the value of x, and the first matching index (if one exists).
+'''
+
+#Starter code given
 from random import random
 values = [random() for i in range(20)]
 x = random()
+
+#Start of written code
 indicesOfValuesEqualOrGreaterThanX = []
 
 values.sort()
@@ -8,4 +19,4 @@ values.sort()
 for i in range(len(values)):
     if values[i] >= x: indicesOfValuesEqualOrGreaterThanX.append(i)
 
-print(values, '\nx: ',x,indicesOfValuesEqualOrGreaterThanX[0])
+print('Sorted list:', values, '\nx: ', x ,'\nFirst matching index:',indicesOfValuesEqualOrGreaterThanX[0])
